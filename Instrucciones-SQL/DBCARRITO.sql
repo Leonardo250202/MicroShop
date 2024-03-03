8db5925d825820eef77ab549673870997a020e5e
Leo 7u7 <153622087+Leonardo250202@users.noreply.github.com>
2024-03-03 12:41:46 -0500

    Validaciones

--- Instrucciones-SQL/DBCARRITO.sql
+++ Instrucciones-SQL/DBCARRITO.sql
@@ -493,3 +493,29 @@ where CONVERT (date, v.FechaVenta) between @fechainicio and @fechafin
 and v.IdTransaccion = iif (@idtransaccion = '', v.IdTransaccion, @idtransaccion)
 
 end
+
+--METODOS_CLIENTES
+
+create proc sp_RegistrarCliente(
+@Nombres varchar (100),
+@Apellidos varchar (100),
+@Correo varchar (100),
+@Clave varchar (100),
+@Mensaje varchar (500) output,
+@Resultado int output
+)
+as
+begin
+	SET @Resultado = 0
+	IF NOT EXISTS (SELECT * FROM CLIENTE WHERE Correo = @Correo)
+begin
+insert into CLIENTE(Nombres, Apellidos, Correo, Clave, Reestablecer) values
+(@Nombres, @Apellidos, @Correo,@Clave,0)
+	
+	SET @Resultado = scope_identity()
+end
+	else
+  set @Mensaje = 'El correo del usuario ya existe'
+end
+
+
