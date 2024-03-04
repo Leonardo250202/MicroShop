e58347ce09bd8c62d11003c7ade751f2b02f9fd1
Andre2607 <133719121+Andre2607@users.noreply.github.com>
2024-03-04 11:35:48 -0500

    update-logiocadepago

--- CursoMVC/CapaPresentacionTienda/Controllers/TiendaController.cs
+++ CursoMVC/CapaPresentacionTienda/Controllers/TiendaController.cs
@@ -9,6 +9,10 @@ using CapaNegocio;
 
 using System.IO;
 using System.Collections;
+using System.Threading.Tasks;
+using System.Data;
+using System.Globalization;
+using System.Runtime.Remoting.Messaging;
 
 namespace CapaPresentacionTienda.Controllers
 {
@@ -207,5 +211,65 @@ namespace CapaPresentacionTienda.Controllers
         public ActionResult Carrito() {
             return View();
         }
+
+        [HttpPost]
+        public async Task<JsonResult> ProcesarPago(List<Carrito> oListaCarrito, Venta oVenta)
+        {
+            decimal total = 0;
+            DataTable detalle_venta = new DataTable();
+            detalle_venta.Locale = new CultureInfo("es-PE");
+            detalle_venta.Columns.Add("IdProducto", typeof(string));
+            detalle_venta.Columns.Add("Cantidad", typeof(int));
+            detalle_venta.Columns.Add("Total", typeof(decimal));
+
+
+            foreach (Carrito oCarrito in oListaCarrito)
+            {
+                decimal subtotal = Convert.ToDecimal(oCarrito.Cantidad.ToString()) * oCarrito.oProducto.Precio;
+
+                total += subtotal;
+
+                detalle_venta.Rows.Add(new object[]
+                {
+                    oCarrito.oProducto.IdProducto,
+                    oCarrito.Cantidad,
+                    subtotal
+                });
+            }
+
+            oVenta.MontoTotal = total;
+            oVenta.IdCliente = ((Cliente)Session["Cliente"]).IdCliente;
+
+            TempData["Venta"] = oVenta;
+            TempData["DetalleVenta"] = detalle_venta;
+
+            return Json(new { Status = true, Link ="/Tienda/Pagofectuado?idTransaccion=code0001&Status=true"}, JsonRequestBehavior.AllowGet);
+            
         }
+
+        public async Task<ActionResult> PagoEfectuado()
+        {
+            string idtransaccion = Request.QueryString["idTransaccion"];
+            bool status = Convert.ToBoolean(Request.QueryString["status"]);
+
+            ViewData["status"] = status;
+
+            if (status)
+            {
+                Venta oVenta = (Venta)TempData["Venta"];
+
+                DataTable detalle_venta = (DataTable)TempData["DetalleVenta"];
+               
+                oVenta.IdTransaccion = idtransaccion;
+
+                string mensaje = string.Empty;
+
+                bool respuesta = new CN_Venta().Registrar(oVenta, detalle_venta, out mensaje);
+
+                ViewData["IdTransaccion"] = oVenta.IdTransaccion;       
+            }
+            return View();
+        }
+
+    }
 }
--- Instrucciones-SQL/DBCARRITO.sql
+++ Instrucciones-SQL/DBCARRITO.sql
@@ -631,3 +631,53 @@ end
 select * from  departamento
 select * from provincia where IdDepartamento = @iddepartamento
 select * from DISTRITO where IdProvincia = @idprovincia and IdDepartamento = @iddepartamento
+
+------------video 42------
+create type [dbo].[EDetalle_Venta] as table(
+	[IdProducto] int null,
+	[Cantidad] int null,
+	[Total] decimal (18,2)
+)
+
+---------
+create procedure usp_RegistrarVenta(
+@IdCIiente int,
+@TotalProducto int,
+@MontoTotal decimal(18,2),
+@Contacto varchar(100),
+@IdDistrito varchar(6),
+@Telefono varchar(10),
+@Direccion varchar(100),
+@IdTransaccion varchar(50),
+@DetalleVenta [EDetalle_Venta] READONLY,
+@Resultado bit output,
+@Mensaje varchar (500) output
+)
+as
+begin
+	begin try
+	declare @idventa int = 0
+	set @Resultado = 1
+	set @Mensaje=''
+
+	begin transaction registro
+
+	insert into VENTA(IdCliente,TotalProducto,MontoTotal,Contacto, IdDistrito,Telefono,Direccion,IdTransaccion)
+	values(@IdCIiente,@TotalProducto,@MontoTotal,@Contacto, @IdDistrito,@Telefono,@Direccion,@IdTransaccion)
+
+		set @idventa = SCOPE_IDENTITY()
+
+		insert into DETALLE_VENTA(IdVenta,IdProducto,Cantidad,Total)
+		select @idventa,IdProducto, Cantidad,Total from @DetalleVenta
+		
+		DELETE FROM CARRITO WHERE IdCliente = @IdCIiente
+
+		commit transaction registro
+		
+	end try
+	begin catch
+		set @Resultado = 0
+		set @Mensaje = ERROR_MESSAGE()
+		rollback transaction registro
+	end catch
+end
